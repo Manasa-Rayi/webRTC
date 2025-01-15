49f1da9f89fbeb44cfb25e5ee0a391195151c48b
Manasa-Rayi <manasa@tektreeinc.com>
2025-01-15 05:37:37 -0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -4,8 +4,8 @@ const { Server } = require("socket.io");
 const io = new Server(8000, {
   cors: {
 
-    origin: "https://your-frontend-url.vercel.app", // Replace with your frontend Vercel URL
-    methods: ["GET", "POST"],
+    origin: "https://web-rtc-ix82.vercel.app/", // Replace with your frontend Vercel URL
+    
     allowedHeaders: ["my-custom-header"],
     credentials: true
   }
@@ -47,35 +47,3 @@ io.on("connection", (socket) => {
 console.log("Backend server running on port 8000...");
 
 
-const emailToSocketIdMap = new Map();
-const socketidToEmailMap = new Map();
-
-io.on("connection", (socket) => {
-  console.log(`Socket Connected`, socket.id);
-  socket.on("room:join", (data) => {
-    const { email, room } = data;
-    emailToSocketIdMap.set(email, socket.id);
-    socketidToEmailMap.set(socket.id, email);
-    io.to(room).emit("user:joined", { email, id: socket.id });
-    socket.join(room);
-    io.to(socket.id).emit("room:join", data);
-  });
-
-  socket.on("user:call", ({ to, offer }) => {
-    io.to(to).emit("incomming:call", { from: socket.id, offer });
-  });
-
-  socket.on("call:accepted", ({ to, ans }) => {
-    io.to(to).emit("call:accepted", { from: socket.id, ans });
-  });
-
-  socket.on("peer:nego:needed", ({ to, offer }) => {
-    console.log("peer:nego:needed", offer);
-    io.to(to).emit("peer:nego:needed", { from: socket.id, offer });
-  });
-
-  socket.on("peer:nego:done", ({ to, ans }) => {
-    console.log("peer:nego:done", ans);
-    io.to(to).emit("peer:nego:final", { from: socket.id, ans });
-  });
-});
